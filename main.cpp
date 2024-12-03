da887be2bc296061a2badf46fd0968493ad623cf
mannyjimen <manueljimenez0320>
2024-12-03 15:11:27 -0500

    compiles on gs

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
-int main()
-{
+#include "File.hpp"
+#include "FileAVL.hpp"
+#include "FileTrie.hpp"
+
+int main(){
     std::cout << "Hello World!\n";
 }
--- solution.cpp
+++ solution.cpp
@@ -8,6 +8,33 @@
 // You must declare them "inline" | declare & implement them at the top of this file, before query()
 // Below query(), implement and document all methods declared in FileTrie.hpp
 
+inline Node* getSupposed(int fileSize, Node* subroot){
+    return subroot;
+}
+
+
+//PRE-ORDER Traverse through the tree. Already given start and end nodes.
+inline void traverseWithPush(Node* start, Node* end, Node* root, std::vector<File*>& finalVector){
+    Node* temp = root;
+}
+ 
+//helper function for prefix function
+inline std::unordered_set<File*> FileTrieSearch(FileTrieNode* current, const std::string& prefix, std::string& currentString){
+    //check if this->stored == prefix (will be base case for recursive function.)
+    currentString = currentString + current->stored;
+    if (currentString == prefix){
+        return current->matching;
+    }
+
+    
+}
+
+inline char nextChar(FileTrieNode* currentTrieNode, const std::string& prefix, const std::string& currentString, int idk){
+    char nextSupposed = prefix[currentString.size() + 1];
+}
+
+
+
 /**
  * @brief Retrieves all files in the FileAVL whose file sizes are within [min, max]
  * 
@@ -18,14 +45,27 @@
         the interval from [max, min] is searched (since max >= min)
  */
 std::vector<File*> FileAVL::query(size_t min, size_t max) {
-    std::vector<File*> result;
-//thoughts:
-//traverse left until we reach a file where left child->getSize() is < min 
-//traverse in order until we reach a file that right child-> getsize() > max
+    std::vector<File*> finalVector;
+    // if (max < min) 
+    //     std::swap(min, max); //will make min the smaller value if min > max
+   
+
+    // Node* startNode = ::getSupposed(min, root_); //get the node we should end at
+    // Node* endNode = ::getSupposed(max, root_); //get the frist node we should start at 
+
+    // traverseWithPush(startNode, endNode, root_, finalVector);
+ 
     // Your code here.
-    return result;
+    return finalVector;
+}
+
+
+void FileTrie::addFile(File* f){
 }
 
 std::unordered_set<File*> FileTrie::getFilesWithPrefix(const std::string& prefix) const{
-
+    std::string currentString = "";
+    std::unordered_set<File*> newSet;
+    // char nextChar = prefix[currentString.size() + 1];
+    return newSet;
 }
