1a7966db7f7b00ddc41e59dd333c02c76871ebc5
mannyjimen <manueljimenez0320@gmail.com>
2024-12-03 22:04:25 -0500

    some alterations

--- solution.cpp
+++ solution.cpp
@@ -8,33 +8,6 @@
 // You must declare them "inline" | declare & implement them at the top of this file, before query()
 // Below query(), implement and document all methods declared in FileTrie.hpp
 
-inline Node* getSupposed(int fileSize, Node* subroot){
-    return subroot;
-}
-
-
-//PRE-ORDER Traverse through the tree. Already given start and end nodes.
-inline void traverseWithPush(Node* start, Node* end, Node* root, std::vector<File*>& finalVector){
-    Node* temp = root;
-}
- 
-//helper function for prefix function
-inline std::unordered_set<File*> FileTrieSearch(FileTrieNode* current, const std::string& prefix, std::string& currentString){
-    //check if this->stored == prefix (will be base case for recursive function.)
-    currentString = currentString + current->stored;
-    if (currentString == prefix){
-        return current->matching;
-    }
-
-    
-}
-
-inline char nextChar(FileTrieNode* currentTrieNode, const std::string& prefix, const std::string& currentString, int idk){
-    char nextSupposed = prefix[currentString.size() + 1];
-}
-
-
-
 /**
  * @brief Retrieves all files in the FileAVL whose file sizes are within [min, max]
  * 
@@ -48,29 +21,23 @@ std::vector<File*> FileAVL::query(size_t min, size_t max) {
     std::vector<File*> finalVector;
     if (max < min) 
         std::swap(min, max); //will make min the smaller value if min > max
-   
 
-    // Node* startNode = ::getSupposed(min, root_); //get the node we should end at
-    // Node* endNode = ::getSupposed(max, root_); //get the frist node we should start at 
 
-    // traverseWithPush(startNode, endNode, root_, finalVector);
- 
-    // Your code here.
     return finalVector;
 }
 
 //Want to add file f to FileTrie
 //thoughts
-
 //add file to the root
 //create new filetrienode with next char, add file to that, keep going until last letter
 //go through the correct FileTrie node, and insert to that node.
 //for adding, head->matching->insert
 
 //auto node_next = root->next['x']
+
 void FileTrie::addFile(File* f){
     const std::string& fname = f->getName();
-    auto temp = head;
+    FileTrieNode* temp = head;
     for (char c : fname){
         c = tolower(c);
         temp->matching.insert(f);    
@@ -84,22 +51,20 @@ void FileTrie::addFile(File* f){
     }
 }
 
-
-//if the next node with the char doesnt exist, return the empty set
+//if the next node with the char doesnt exist, return empty set of file pointers
 //once we get to the end of the prefix, return the matching set for where the temp pointer is at. 
 std::unordered_set<File*> FileTrie::getFilesWithPrefix(const std::string& prefix) const{
-    auto temp = head;
-    // char nextChar = prefix[currentString.size() + 1];
-    for (int i = 0; i < prefix.size(); i++){
+    FileTrieNode* temp = head;
+    std::unordered_set<File*> emptySet;
+    for (size_t i = 0; i < prefix.size(); i++){
         char c = tolower(prefix[i]);
         if (temp->next[c]){
             if (i == prefix.size() - 1)
                 return temp->matching;
             temp = temp->next[c];
         }
-        else{
-            std::unordered_set<File*> emptySet;
+        else
             return emptySet;
-        }
     }
+    return emptySet;
 }
