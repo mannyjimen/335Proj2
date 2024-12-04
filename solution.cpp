95f2e8ad19a012eb6d607a9639572699d4eed5b1
mannyjimen <manueljimenez0320>
2024-12-03 21:33:55 -0500

    getting close

--- solution.cpp
+++ solution.cpp
@@ -46,8 +46,8 @@ inline char nextChar(FileTrieNode* currentTrieNode, const std::string& prefix, c
  */
 std::vector<File*> FileAVL::query(size_t min, size_t max) {
     std::vector<File*> finalVector;
-    // if (max < min) 
-    //     std::swap(min, max); //will make min the smaller value if min > max
+    if (max < min) 
+        std::swap(min, max); //will make min the smaller value if min > max
    
 
     // Node* startNode = ::getSupposed(min, root_); //get the node we should end at
@@ -59,13 +59,47 @@ std::vector<File*> FileAVL::query(size_t min, size_t max) {
     return finalVector;
 }
 
+//Want to add file f to FileTrie
+//thoughts
 
+//add file to the root
+//create new filetrienode with next char, add file to that, keep going until last letter
+//go through the correct FileTrie node, and insert to that node.
+//for adding, head->matching->insert
+
+//auto node_next = root->next['x']
 void FileTrie::addFile(File* f){
+    const std::string& fname = f->getName();
+    auto temp = head;
+    for (char c : fname){
+        c = tolower(c);
+        temp->matching.insert(f);    
+        if (temp->next[c]) //if does exist, make temp that
+            temp = temp->next[c];
+        else{ //next char doesnt exist
+            FileTrieNode* newNode = new FileTrieNode(c);
+            temp->next[c] = newNode;
+            temp = temp->next[c];
+        }
+    }
 }
 
+
+//if the next node with the char doesnt exist, return the empty set
+//once we get to the end of the prefix, return the matching set for where the temp pointer is at. 
 std::unordered_set<File*> FileTrie::getFilesWithPrefix(const std::string& prefix) const{
-    std::string currentString = "";
-    std::unordered_set<File*> newSet;
+    auto temp = head;
     // char nextChar = prefix[currentString.size() + 1];
-    return newSet;
+    for (int i = 0; i < prefix.size(); i++){
+        char c = tolower(prefix[i]);
+        if (temp->next[c]){
+            if (i == prefix.size() - 1)
+                return temp->matching;
+            temp = temp->next[c];
+        }
+        else{
+            std::unordered_set<File*> emptySet;
+            return emptySet;
+        }
+    }
 }
